99b58477061c4552d8174c415b65f3f0d1603ebb
AjayaDesign <ajayadesign@gmail.com>
2026-04-28 19:12:09 -0500

    Enlarge crop modal and maximize crop box coverage

--- assets/js/pdp-cropper.js
+++ assets/js/pdp-cropper.js
@@ -142,7 +142,7 @@
       aspectRatio: currentAspectRatio || NaN,
       viewMode: 1,
       dragMode: 'move',
-      autoCropArea: 0.9,
+      autoCropArea: 1,
       responsive: true,
       restore: false,
       guides: true,
